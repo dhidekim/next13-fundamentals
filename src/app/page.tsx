35aa8264a23ac8cfd72b784e6f75f7eb2928995d
Douglas <dhidekim@gmail.com>
2025-03-03 15:51:26 -0300

    Fundamentos App Router - Fetch de dados nos componentes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,8 @@
-export default function Home() {
-  return <h1>Hello</h1>
+export default async function Home() {
+  await new Promise((resolve) => setTimeout(resolve, 2000))
+
+  const response = await fetch('https://api.github.com/users/dhidekim')
+  const user = await response.json()
+
+  return <pre>{JSON.stringify(user, null, 2)}</pre>
 }
